68bba0bb296e1d2b9b3219411a797f339df38a0c
EC2 Default User <ec2-user@ip-172-31-37-69.ap-northeast-1.compute.internal>
2020-12-10 13:59:41 +0000

    リサイズコマンド打つまで

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -22,11 +22,8 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-<<<<<<< HEAD
   # process resize_to_fit: [200, 300]
-=======
   # process scale: [200, 300]
->>>>>>> 0103ccb697660589c1b76f740defa6e2dfd5804e
   #
   # def scale(width, height)
   #   # do something
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,7 +3,7 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
-<<<<<<< HEAD
+
     config.storage :fog
     config.fog_provider = 'fog/aws'
     config.fog_directory  = 'anchorpictures' # 作成したバケット名を記述
@@ -15,17 +15,4 @@ CarrierWave.configure do |config|
       path_style: true
     }
 end 
-=======
-  config.storage :fog
-  config.fog_provider = 'fog/aws'
-  config.fog_directory  = 'anchorsystem'
-  config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-    region: ENV['AWS_DEFAULT_REGION'],
-    path_style: true
-  }
 
-end
->>>>>>> 0103ccb697660589c1b76f740defa6e2dfd5804e
